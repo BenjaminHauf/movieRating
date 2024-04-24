f89f69283e621c5f15691a0f87dcd0325dc73c54
javierdv00 <dv.javier@gmail.com>
2024-04-24 15:10:16 +0200

    erick project

---
+++ erick_project/recommendations.py
@@ -0,0 +1,47 @@
+import openai
+from .models import Rating
+
+
+openai.api_key = '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'
+
+
+# USING AI TO GET RESPONSE
+def get_chatgpt_response(user_input, reco_num_gpt, completions:int):
+    openai.api_key = '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 ' #'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'
+    response = openai.Completion.create(engine="gpt-3.5-turbo-instruct", 
+    prompt=user_input,
+    max_tokens=80*reco_num_gpt,
+    temperature=1.0,
+    n=completions,
+    echo=False,
+    presence_penalty=0.5,
+    frequency_penalty=0.5,
+    stream = False)
+    choices = []
+    for i in range(completions):
+        choices.append(response.choices[i].text.strip())
+    return choices
+
+def select_movies_liked(user):
+    ratings = Rating.objects.filter(user=user, stars__gte=4)
+    movies_liked = [rating.movie for rating in ratings]
+    return movies_liked
+
+def generate_recommendations(user, reco_num_gpt):
+    movies_liked = select_movies_liked(user)
+    movies_str = ', '.join([movie.title for movie in movies_liked])
+    text_input = f"""
+    Can you recommend me a list of {reco_num_gpt} new movies that I should watch based on the movies that I liked or loved?
+    And also explain a little the reason, why should I like the recommended movies.
+    This are the movies that I liked: {movies_str}
+    """
+    ##----- With this part the code retrun a string output not a list with 1 value and also informa if there some error when try to connect to chat GPT ----------###############
+    response = ''
+    try:
+        for i in get_chatgpt_response(text_input, reco_num_gpt, 1):
+            response += i
+    except Exception as e:
+        response = 'Failed to communicate with Chat GPT: \n{}'.format(str(e))
+
+    return response
+    
---
+++ erick_project/views.py
@@ -0,0 +1,101 @@
+from django.shortcuts import render, redirect
+from django.contrib.auth.forms import AuthenticationForm
+from django.contrib.auth.decorators import login_required
+from django.contrib.auth import login, logout
+from .models import Movie, Rating
+from .forms import RatingForm, RegistrationForm, MovieForm
+from django.shortcuts import get_object_or_404
+from .recommendations import generate_recommendations
+from django.db.models import Avg
+# Create your views here.
+
+@login_required
+def movie_list(request):
+    if request.method == 'POST':
+        form = MovieForm(request.POST)
+        if form.is_valid():
+            movie = form.save(commit=False)
+            movie.user = request.user
+            movie.save()
+            return redirect('movie_list') 
+    else:
+        form = MovieForm()
+
+    movies = Movie.objects.filter(user=request.user)
+    user_ratings = {}
+    
+    for movie in movies:
+        rating = movie.rating_set.filter(user=request.user).first()
+        user_ratings[movie.id] = rating.stars if rating else None
+
+     # Recommendation logic
+    reco_num_gpt = 3  # Number of recommended movies
+    recommendations = generate_recommendations(request.user, reco_num_gpt)
+    recommendations = recommendations.replace('\n', '<br>')         # To change the new line in python(\n) to new line in html (<br>)
+    
+    context = {
+        'movies': movies,
+        'user_ratings': user_ratings,
+        'user': request.user,
+        'recommendations': recommendations
+    }
+
+    return render(request, 'ratings/movie_list.html', context)
+
+
+@login_required
+def rate_movie(request, movie_id):
+    movie = Movie.objects.get(pk=movie_id)
+    user_rating = Rating.objects.filter(movie=movie, user=request.user).first()
+
+    if request.method == 'POST':
+        form = RatingForm(request.POST, instance=user_rating)
+        if form.is_valid():
+            rating = form.save(commit=False)
+            rating.user = request.user
+            rating.movie = movie
+            rating.save()
+
+            # Recalculate average rating for the movie
+            avg_rating = Rating.objects.filter(movie=movie).aggregate(Avg('stars'))['stars__avg']
+            movie.avg_rating = avg_rating
+            movie.save()
+
+            return redirect('movie_list')
+
+    else:
+        form = RatingForm(instance=user_rating)
+
+    return render(request, 'ratings/rate_movie.html', {'form': form, 'movie': movie})
+
+
+def login_view(request):
+    if request.method == 'POST':
+        form = AuthenticationForm(request, request.POST)
+        if form.is_valid():
+            login(request, form.get_user())
+            return redirect('movie_list')  # Redirect to the home page or any other desired page
+    else:
+        form = AuthenticationForm()
+    return render(request, 'ratings/login.html', {'form': form})
+
+def logout_view(request):
+    logout(request)
+    return redirect('login')  # Redirect to the login page after logout
+
+
+def register(request):
+    if request.method == 'POST':
+        form = RegistrationForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('login')  # Redirect to the login page after successful registration
+    else:
+        form = RegistrationForm()
+    return render(request, 'ratings/register.html', {'form': form})
+
+def home(request):
+    if request.user.is_authenticated:
+        return redirect('movie_list')
+    return redirect('login')
+
